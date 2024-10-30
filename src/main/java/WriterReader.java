8c8e47d57a454cd4e03fa18099412ae2b37fa02c
OlenaShcherbina <olenas.shcherbina@gmail.com>
2024-10-30 09:06:18 +0100

    initial commit

---
+++ src/main/java/WriterReader.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class WriterReader {
+
+  private static final String FILE_NAME = "output.txt";
+
+  private static final String EXIT = "-q";
+
+  private static final String LINE_SEPARATOR = System.lineSeparator();
+
+  public static void main(String[] args) throws IOException {
+
+    // TODO write your code here...
+
+    System.out.println("===================");
+    System.out.println("Content written: ");
+    System.out.println();
+
+    // TODO step 5 goes here
+
+    System.out.println("===================");
+    System.out.println("Exiting...");
+  }
+}
