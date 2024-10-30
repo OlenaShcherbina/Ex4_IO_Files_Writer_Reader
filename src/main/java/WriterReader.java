4dce2e8125cf93919e08003ba8c3452894b7699a
OlenaShcherbina <olenas.shcherbina@gmail.com>
2024-10-30 10:31:03 +0100

    added my solution

--- src/main/java/WriterReader.java
+++ src/main/java/WriterReader.java
@@ -1,4 +1,11 @@
-import java.io.IOException;
+import jdk.dynalink.StandardOperation;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
 public class WriterReader {
 
@@ -8,17 +15,54 @@ public class WriterReader {
 
   private static final String LINE_SEPARATOR = System.lineSeparator();
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
 
     // TODO write your code here...
+    Path filePath = Paths.get(FILE_NAME);
+    try {
+      if (Files.exists(filePath)) {
+        Files.delete(filePath);
+      }
+    } catch (IOException e) {
+      System.out.println("Failed file deleting" + e.getMessage());
+    }
+    try {
+      Files.createFile(filePath);
+    } catch (IOException e) {
+      System.out.println("Failed file creation" + e.getMessage());
+    }
 
-    System.out.println("===================");
-    System.out.println("Content written: ");
-    System.out.println();
+    try (BufferedReader theKeyboard = new BufferedReader(new InputStreamReader(System.in))) {
+      String input = "";
 
-    // TODO step 5 goes here
+      while(true){
+        System.out.println("Write a sentence to be written in the file: ");
+        input = theKeyboard.readLine();
+        if(!input.equals(EXIT)) {
+          Files.writeString(filePath, input + LINE_SEPARATOR, StandardOpenOption.APPEND);
+        }else{
+          System.out.println("Exiting the loop..");
+          break;
+        }
+      }
 
-    System.out.println("===================");
-    System.out.println("Exiting...");
+      System.out.println("===================");
+      System.out.println("Content written: ");
+      System.out.println();
+
+      // TODO step 5 goes here
+
+      List<String> fileContent = Files.readAllLines(filePath);
+      for(String line: fileContent){
+        System.out.println(line);
+      }
+
+
+      System.out.println("===================");
+      System.out.println("Exiting...");
+
+    }catch(IOException e){
+      e.getStackTrace();
+    }
   }
 }
